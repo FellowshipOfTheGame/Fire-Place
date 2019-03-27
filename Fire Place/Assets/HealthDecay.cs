02d558bd6d7d10731e6fd9e2f006c21184ad4b73
fergorgs <fer.fayet@usp.br>
2019-03-26 21:15:15 -0300

    Movimentação + mecanica do gelo

---
+++ Fire Place/Assets/HealthDecay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDecay : MonoBehaviour
+{
+	public GameObject player;
+
+	public float alpha = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		alpha = (1 - player.GetComponent<PlayerBehavior>().getHealth()/100) * 0.6f;
+
+		GetComponent<Image>().color = new Color(0, 0, 0, alpha);
+    }
+}
--- Fire Place/Assets/PlayerBehavior.cs
+++ Fire Place/Assets/PlayerBehavior.cs
@@ -4,43 +4,94 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-	public float movementForce = 2f;
+	public float acceleration = 2f;
+	public float maxVelocity = 8.5f;
+	public float gravityScale = 1;
+	public float iceDamage = 1;
+
+	private bool takingDamage = true;
+	public bool isTakingDamage() { return takingDamage; }
+	public void setTakingDamage(bool val) { takingDamage = val; }
+
+	private float health = 100;
+	public float getHealth() { return health; }
+
+	private float extraGravity = 0;
 
 	private Rigidbody rgbd;
 
     // Start is called before the first frame update
     void Start()
     {
-		rgbd = GetComponent<Rigidbody>();
-        
+		extraGravity = gravityScale - 1;
+		rgbd = GetComponent<Rigidbody>();        
     }
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.LeftArrow))
+		//GRAVITY SCALE----------------------------------------------------------------------------
+		extraGravity = gravityScale - 1;							//adiciona gravidade extra
+		rgbd.AddForce(Physics.gravity * extraGravity);
+
+		//MOVEMENT INPUT---------------------------------------------------------------------------
+		if (Vector3.Magnitude(rgbd.velocity) <= maxVelocity)                                //se a velocidade não é máxima
 		{
-			//rgbd.velocity = Vector3.zero;
-			rgbd.AddRelativeForce(new Vector3(-1, 0, 0) * movementForce, ForceMode.Force);
-			Debug.Log("Get it");
+			if (Input.GetKey(KeyCode.LeftArrow))                                                     //Adiciona força de acordo com
+				rgbd.AddForce(new Vector3(-1, 0, 0) * acceleration, ForceMode.Force);           //o eixo de input
+
+			if (Input.GetKey(KeyCode.RightArrow))
+				rgbd.AddForce(new Vector3(1, 0, 0) * acceleration, ForceMode.Force);
+
+			if (Input.GetKey(KeyCode.UpArrow))
+				rgbd.AddForce(new Vector3(0, 0, 1) * acceleration, ForceMode.Force);
+
+			if (Input.GetKey(KeyCode.DownArrow))
+				rgbd.AddForce(new Vector3(0, 0, -1) * acceleration, ForceMode.Force);
 		}
 
-		else if (Input.GetKey(KeyCode.RightArrow))
+		//ROTATION----------------------------------------------------------------------------------
+		/*float yAngle = Vector3.Angle(new Vector3(rgbd.velocity.x, 0, 0), new Vector3(0, 0, rgbd.velocity.z));
+
+		transform.eulerAngles = new Vector3(0, yAngle, 0);*/
+
+
+
+		//ICE DAMAGE-------------------------------------------------------------------------------
+
+		if (takingDamage)
 		{
-			//rgbd.velocity = Vector3.zero;
-			rgbd.AddRelativeForce(new Vector3(1, 0, 0) * movementForce, ForceMode.Force);
+			if (health > 40)
+				health -= 3 * iceDamage / 100;
+			else
+				health -= iceDamage / 100;
+
+		}
+		else if (health < 100)
+			health += 5 * iceDamage / 100;
+
+		if (health <= 0)
+			Debug.Log("Dead");
+
+
+		if (Input.GetKey(KeyCode.R))
+		{
+
+			transform.position = new Vector3(42, 9, 22);
 		}
 
-		else if (Input.GetKey(KeyCode.UpArrow))
-		{
-			//rgbd.velocity = Vector3.zero;
-			rgbd.AddRelativeForce(new Vector3(0, 0, 1) * movementForce, ForceMode.Force);
-		}
+		Debug.Log("Health = " + health);
+	}
 
-		else if (Input.GetKey(KeyCode.DownArrow))
-		{
-			//rgbd.velocity = Vector3.zero;
-			rgbd.AddRelativeForce(new Vector3(0, 0, -1) * movementForce, ForceMode.Force);
-		}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Fire")
+			takingDamage = false;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Fire")
+			takingDamage = true;
 	}
 }
