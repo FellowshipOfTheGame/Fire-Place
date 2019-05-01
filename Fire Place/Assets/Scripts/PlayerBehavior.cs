24468fc46ba93f95a8d00e6acceb6bb86f151ca2
Luis Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2019-05-01 20:09:04 -0300

    Finalized merge and gameplay. - Modified scenes to make some gameplay possible. - Fixed (a bit) the Test Field scene. - Added Vulkan as Windows default API. - Changed rendering mode to forward. - Removed some packages that probably won't be used. - Added FXAA. - Added Alan Turing. - Better organized files in the project. - Changed some materials in order to reduce their amount. - Added animated crossfade to some missing LODs.
    OBS: Find a better way to move the player according to the camera (current one may apply force on the z-axis if camera is tilted).

---
+++ Fire Place/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+	public enum States { Default, Lendo, Fogueira };
+
+	private States state;
+	public void setState(States state) { this.state = state; }
+	public States getState() { return state; }
+
+	public float acceleration = 2f;
+	public float maxVelocity = 8.5f;
+	public float gravityScale = 1;
+
+	private float extraGravity = 0;
+
+	private Transform mainCamera;
+
+	private Rigidbody rgbd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		mainCamera = Camera.main.transform;
+
+		extraGravity = gravityScale - 1;
+		rgbd = GetComponent<Rigidbody>();
+
+		state = States.Default;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		switch (state)
+		{
+			case States.Default:
+
+				//MOVEMENT INPUT---------------------------------------------------------------------------
+				if (Vector3.Magnitude(rgbd.velocity) <= maxVelocity)                                //se a velocidade não é máxima
+				{
+
+					float horAxis = Input.GetAxis("Horizontal");
+					float verAxis = Input.GetAxis("Vertical");
+
+					if (horAxis != 0)                                                     //Adiciona força de acordo com
+						rgbd.AddForce(mainCamera.right * Mathf.Sign(horAxis) * acceleration, ForceMode.Force);           //o eixo de input
+
+					if (verAxis != 0)
+						rgbd.AddForce(mainCamera.forward * Mathf.Sign(verAxis) * acceleration, ForceMode.Force);
+				}
+
+				break;
+		}
+
+		
+		//GRAVITY SCALE----------------------------------------------------------------------------
+		extraGravity = gravityScale - 1;							//adiciona gravidade extra
+		rgbd.AddForce(Physics.gravity * extraGravity);
+
+
+		//ROTATION----------------------------------------------------------------------------------
+		Vector3 projection = new Vector3(rgbd.velocity.x, 0, rgbd.velocity.z);
+		float yAngle = Vector3.Angle(projection, new Vector3(1, 0, 0));
+
+		if (Vector3.Magnitude(rgbd.velocity) > 1)
+		{
+			if (rgbd.velocity.z > 0)
+				transform.eulerAngles = new Vector3(0, -yAngle, 0);
+			else
+				transform.eulerAngles = new Vector3(0, yAngle, 0);
+		}
+
+		if (Input.GetKey(KeyCode.R))
+		{
+
+			transform.position = new Vector3(42, 9, 22);
+		}
+
+	}
+}
