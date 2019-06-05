10088a3f3ecaeb66c28f9c9ae3e132239f5dcc94
Luis Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2019-06-05 11:09:44 -0300

    Added camera changes to scenes.
    - Also an FPS counter was added (Press F8 in game when running a build).

---
+++ Fire Place/Assets/Scripts/Camera/OnTriggerSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class OnTriggerSwitch : MonoBehaviour
+{
+
+    [SerializeField] private CinemachineVirtualCamera toggleCam = null;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Player")
+        {
+
+            //cam.enabled = false;
+            toggleCam.enabled = true;
+
+        }
+
+    }
+
+}
---
+++ Fire Place/Assets/Scripts/Util/FPSCounter.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+
+    private bool show = false;
+    private float fps = 0;
+
+    private Coroutine calculateFPS = null;
+
+    void Start() 
+    {
+
+        
+
+    }
+
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.F8)) show =! show;
+
+        if(show) 
+        {
+            if(calculateFPS == null)
+            {
+                fps = 1 / Time.deltaTime;
+                calculateFPS = StartCoroutine(CalculateFPS());
+            }
+        }
+        else
+        {
+
+            if(calculateFPS != null)
+            {
+                StopCoroutine(calculateFPS);
+                calculateFPS = null;
+            }
+
+        }
+
+    }
+
+    IEnumerator CalculateFPS() 
+    {
+
+        // Samples FPS 100 times and them updates the counter with the average.
+        float secFrames = 0;
+        float counter = 0;
+
+        while(show)
+        {
+
+            if(counter == 100)
+            {
+
+                fps = 1 / (secFrames / 100);
+                secFrames = 0;
+                counter = 0;
+
+            } 
+            else
+            {
+
+                secFrames += Time.deltaTime;
+                counter++;
+
+            }
+
+            yield return null;
+
+        }
+
+    }
+
+    void OnGUI()
+    {
+
+
+        if(show)
+            GUI.Label(new Rect(10,10,200,200), "FPS: " + Mathf.Floor(fps));
+
+    }
+
+}
