5633d8c04d54a4afa795be6ab6f1e94ac5fd571a
fergorgs <fer.fayet@usp.br>
2019-06-05 00:19:17 -0300

    efeito inicial com a camera principal

--- Fire Place/Assets/MenuPrincipalScript.cs
+++ Fire Place/Assets/MenuPrincipalScript.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuPrincipalScript : MonoBehaviour
 {
 	public Material BlackPlaneMaterial;
-
-	public GameObject btnJogar, btnContinuar, textJogar, textContinuar;
-
+	public GameObject blackPanel, textJogar, assistCamera;
+	public GameObject mainCamera;
 	public float fadeSpeed = 1f;
-
 	private bool fadding = false;
+	private Color curColorBlack, curColorWhite;
 
-	private Color curColorBlack, curColorWhite, curColorGray;
+	public string playerSceneName = "Player";
 
 	float startTime;
 
@@ -24,39 +24,40 @@ public class MenuPrincipalScript : MonoBehaviour
 
 		curColorBlack = Color.black;
 		curColorWhite = Color.white;
-		curColorGray = textJogar.GetComponent<Text>().color;
+
+		mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+		assistCamera.transform.position = mainCamera.transform.position;
+		assistCamera.transform.rotation = mainCamera.transform.rotation;
 	}
 
     // Update is called once per frame
     void Update()
     {
+		assistCamera.transform.position = mainCamera.transform.position;
+		assistCamera.transform.rotation = mainCamera.transform.rotation;
+
 		float t;
 		if (fadding)
 		{
 			t = (Time.time - startTime) * fadeSpeed;
 			curColorBlack = Color.Lerp(curColorBlack, Color.clear, t);
 			curColorWhite = Color.Lerp(curColorWhite, Color.clear, t);
-			curColorGray = Color.Lerp(curColorGray, Color.clear, t);
 
 			BlackPlaneMaterial.SetColor("_BaseColor", curColorBlack);
-			btnJogar.GetComponent<Image>().color = curColorWhite;
-			btnContinuar.GetComponent<Image>().color = curColorWhite;
-			textJogar.GetComponent<Text>().color = curColorGray;
-			textContinuar.GetComponent<Text>().color = curColorGray;
+			textJogar.GetComponent<Text>().color = curColorWhite;
 
-			if(curColorWhite.a < 0.1)
+			if (curColorWhite.a < 0.1)
 			{
-				btnJogar.SetActive(false);
-				btnContinuar.SetActive(false);
+				blackPanel.SetActive(false);
+				textJogar.SetActive(false);
 			}
 		}
+
+		if (Input.anyKeyDown)
+		{
+			fadding = true;
+			startTime = Time.time;
+		}
     }
-
-	public void StartNewGame()
-	{
-		fadding = true;
-		startTime = Time.time;
-	}
-
-
 }
