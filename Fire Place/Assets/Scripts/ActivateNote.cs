d6ece6dfcbf0a470f92250719bba9bf80f736874
luis.eduardo.rozante@gmail.com <luis.eduardo.rozante@gmail.com>
2019-07-08 12:30:45 -0300

    Scenery, atlas and notes.
    Additions to scenery.
    
    Changed most materials to use a texture atlas.
    
    Added more notes to the scenery.
    
    Fixed note not using using the 'E' key instead of Use input (caused problens when using a controller).
    
    Optimized Notes by storing some values in variables minimizing the use of Finds and Gets.
    
    Ensured that Notes will always be set to Peserve Aspect.

--- Fire Place/Assets/Scripts/ActivateNote.cs
+++ Fire Place/Assets/Scripts/ActivateNote.cs
@@ -5,89 +5,105 @@ using UnityEngine.UI;
 
 public class ActivateNote : MonoBehaviour
 {
-	public GameObject letterE;
-	public GameObject MainCamera;
-	public GameObject noteOnCanvas;
+	public MeshRenderer letterE;
 
 	public Sprite noteImage;
 	public string noteText;
-	//public Text test;
+
+	private Image noteOnCanvasImage;
+	private Text noteOnCanvasText;
 
 	private bool showing = false;
 	public bool isShowing() { return showing; }
+
 	private bool inRange = false;
-	public GameObject player;
+
+	public PlayerBehaviour player;
 
 	void Start()
 	{
 		//noteOnCanvas.SetActive(true);
-		noteOnCanvas = GameObject.FindGameObjectWithTag("noteOnCanvas");
-		player = GameObject.FindGameObjectWithTag("Player");
-		MainCamera = Camera.main.gameObject;
+		GameObject noteOnCanvas = GameObject.FindGameObjectWithTag("noteOnCanvas");
 
-		noteOnCanvas.GetComponentInChildren<Image>().enabled = false;
-		noteOnCanvas.GetComponentInChildren<Text>().enabled = false;
+		noteOnCanvasImage = noteOnCanvas.GetComponentInChildren<Image>();
+		noteOnCanvasText = noteOnCanvas.GetComponentInChildren<Text>();
+
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+
+		noteOnCanvasImage.enabled = false;
+		noteOnCanvasText.enabled = false;
 
 	}
 	void Update()
 	{
-		//letterE.transform.LookAt(MainCamera.transform);
 
 		if (inRange)
 		{
 			if (!showing) {
-				if (Input.GetKeyDown(KeyCode.E))
+
+				if (Input.GetButtonDown("Use"))
 				{
 					//Debug.Log("Entra");
-					showing = true;
-					player.GetComponent<PlayerBehaviour>().setState(PlayerBehaviour.States.Lendo);
-
-					noteOnCanvas.GetComponentInChildren<Image>().sprite = noteImage;
-					noteOnCanvas.GetComponentInChildren<Text>().text = noteText;
-
-					noteOnCanvas.GetComponentInChildren<Image>().enabled = true;
-					noteOnCanvas.GetComponentInChildren<Text>().enabled = true;
-
-
+					ToogleNote(true);
 				}
-			}
-			else {
+
+			} else {
+
 				if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Use"))
 				{
 					//Debug.Log("Sai");
-					showing = false;
-					player.GetComponent<PlayerBehaviour>().setState(PlayerBehaviour.States.Default);
-
-					noteOnCanvas.GetComponentInChildren<Image>().enabled = false;
-					noteOnCanvas.GetComponentInChildren<Text>().enabled = false;
+					ToogleNote(false);
 				}
+
 			}
-		}	
+		}
+
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
+
 		if(other.tag == "Player")
 		{
-			letterE.GetComponent<MeshRenderer>().enabled = true;
+			letterE.enabled = true;
 			inRange = true;
-		}	
+		}
+
 	}
 
 	void OnTriggerExit(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			letterE.GetComponent<MeshRenderer>().enabled = false;
+
+			letterE.enabled = false;
 			inRange = false;
 		}
+
 	}
 
-	private void showNote(bool show)
+	private void ToogleNote(bool show)
 	{
+
+		showing = show;
 
 		if (show)
 		{
+
+			player.setState(PlayerBehaviour.States.Lendo);
+
+			noteOnCanvasImage.sprite = noteImage;
+			noteOnCanvasImage.preserveAspect = true;
+			noteOnCanvasText.text = noteText;
+
+		} else {
+
+			player.setState(PlayerBehaviour.States.Default);
+
 		}
+
+		noteOnCanvasImage.enabled = show;
+		noteOnCanvasText.enabled = show;
+
 	}
 }
